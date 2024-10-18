cc7c051829ff5b0e21b87e6d2a7882f4cc2217df
TheodoreUtomo <theodoreutomo@gmail.com>
2024-10-18 14:42:06 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Theodore Utomo on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAllTests/CatchEmAllTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllTests.swift
+//  CatchEmAllTests
+//
+//  Created by Theodore Utomo on 10/18/24.
+//
+
+import Testing
+@testable import CatchEmAll
+
+struct CatchEmAllTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
